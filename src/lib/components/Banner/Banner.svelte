59cbe558dd7db60d64b1a150659805f20ecaf87a
PhubestSrikooon <forbf100@gmail.com>
2023-11-17 22:33:10 +0700

    3js

---
+++ src/lib/components/Banner/Banner.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import * as THREE from 'three';
+
+    onMount(()=>{
+        
+    })
+</script>
--- src/routes/(content)/+page.svelte
+++ src/routes/(content)/+page.svelte
@@ -1,9 +1,12 @@
 <script>
+
     import {t,locales,locale,supportedLocales,saveLanguage} from "$lib/translation";
     import { enhance } from "$app/forms";
     import LanguageSwitcher from "$lib/components/LanguageSwitcher.svelte";
     import ThemeSwitcher from "$lib/components/ThemeSwitcher.svelte";
+    import Banner from "$lib/components/Banner/Banner.svelte";
 </script>
+<Banner />
 <p>{$t("common.home.title")}</p>
 <p>{$t("common.home.subtitle",{name:"Best!"})}</p>
 <a href="/settings" class="btn btn-accent">
